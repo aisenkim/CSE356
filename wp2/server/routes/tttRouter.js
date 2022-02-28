eb71a3c7acff1ff2f32fd78a0b09922a9e52deb2
Aisen Kim <aisenkim15@gmail.com>
2022-02-27 23:42:25 -0500

    added a get request

--- wp2/server/routes/tttRouter.js
+++ wp2/server/routes/tttRouter.js
@@ -1,14 +1,15 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const Auth = require("../middleware/authMiddleware")
+const Auth = require("../middleware/authMiddleware");
 
-const tttController = require("../controller/tttController")
+const tttController = require("../controller/tttController");
 
-router.get("/", tttController.helloWorld)
-router.post("/ttt/", tttController.postName)
-router.post("/ttt/play", Auth.isAuthenticated, tttController.playGame)
-router.post("/listgames", Auth.isAuthenticated, tttController.listGame)
-router.post("/getgame", Auth.isAuthenticated, tttController.getGame)
-router.post("/getscore", Auth.isAuthenticated, tttController.getScore)
+router.get("/", tttController.helloWorld);
+router.get("/ttt/play", tttController.getPlay);
+router.post("/ttt/", tttController.postName);
+router.post("/ttt/play", Auth.isAuthenticated, tttController.playGame);
+router.post("/listgames", Auth.isAuthenticated, tttController.listGame);
+router.post("/getgame", Auth.isAuthenticated, tttController.getGame);
+router.post("/getscore", Auth.isAuthenticated, tttController.getScore);
 
-module.exports = router
+module.exports = router;
